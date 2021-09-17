336dc12e054977a1c366e64304d9a1f7067d4fa5
seriko87 <58387052+seriko87@users.noreply.github.com>
2021-09-17 14:41:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [selectedUser, setSelectedUser] = useState([]);
+  const [btnSelected, setBtnSelected] = useState(false);
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((emp) => {
+        setUser(emp);
+        setIsLoading(false);
+        setSelectedUser(emp[0]);
+        setBtnSelected(emp[0].id);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleClick = (id) => {
+    setSelectedUser(() => {
+      const newUser = user.filter((e) => e.id === id);
+      return newUser[0];
+    });
+    setBtnSelected(id);
+  };
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Experience</h1>
+      <div className='container'>
+        <section className='company'>
+          {user.map((items) => {
+            return (
+              <button
+                onClick={() => handleClick(items.id)}
+                className={`btn ${
+                  btnSelected === items.id ? "active-btn" : false
+                }`}
+              >
+                {items.company}
+              </button>
+            );
+          })}
+        </section>
+        <section className='user'>
+          <div>
+            <h2>{selectedUser.title}</h2>
+            <p className='p-company'>{selectedUser.company}</p>
+            <p className='p-dates'>{selectedUser.dates}</p>
+            {selectedUser.duties ? (
+              selectedUser.duties.map((item) => {
+                return (
+                  <p className='info'>
+                    <FaAngleDoubleRight /> {item}
+                  </p>
+                );
+              })
+            ) : (
+              <div></div>
+            )}
+          </div>
+
+          <button className='moreInfo'>More Info</button>
+        </section>
+      </div>
+    </main>
   );
 }
 
